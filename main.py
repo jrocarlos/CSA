f254453ba85f645c2ddc3b6450289337c0845160
jrocarlos <75398058+jrocarlos@users.noreply.github.com>
2022-07-12 12:02:33 -0300

    Configure module update

--- main.py
+++ main.py
@@ -8,24 +8,23 @@ rm = pyvisa.ResourceManager()
 inst = rm.open_resource('TCPIP::10.1.14.123')
 print(inst.query("*IDN?"))
 
-R = MB(0, "Você deseja confirgurar o equipamento?", "Configuração", 1)
+R = MB(0, "Você deseja confirgurar o equipamento?", "Configuração", 4)
 
-if R == 1:
+if R == 6:
     MB(0, "Configurando","Setting", 0)
-    time.sleep(10)
+    #Config
+    inst.write("*RST")
+    time.sleep(5)
+    inst.write(":INST:SEL SR")
+    inst.write(":CONF:TPIL")
+    time.sleep(5)
+    inst.write("FREQ:STAR 10 MHz")
+    inst.write("FREQ:STOP 100 MHz")
+    inst.write("INIT:CONT ON")
+    time.sleep(5)
+    inst.write("TPIL:AVER:COUN 10")
+    print("URRA!")
+    time.sleep(5)
 else:
     MB(0, "FIM","Setting", 0)
 
-#Config
-inst.write("*RST")
-
-inst.write(":INST:SEL SR")
-inst.write(":CONF:TPIL")
-
-inst.write("FREQ:STAR 10 MHz")
-inst.write("FREQ:STOP 100 MHz")
-inst.write("INIT:CONT ON")
-
-inst.write("TPIL:AVER:COUN 10")
-
-print("URRA!")
