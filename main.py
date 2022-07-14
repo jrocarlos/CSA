b7725c36d23cf0140ac7dbd92fc833a4aef59c7a
jrocarlos <75398058+jrocarlos@users.noreply.github.com>
2022-07-14 14:47:54 -0300

    manual normalization

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import time
 import ctypes  
 
 MB = ctypes.windll.user32.MessageBoxW
+
 #Open comunication
 rm = pyvisa.ResourceManager()
 inst = rm.open_resource('TCPIP::10.1.14.123')
+
 #verify the communication
 idn = inst.query("*IDN?")
 if idn.find('N1996A'):
     MB(0,"Equipment connected!", "Connection",0)
 else:
     MB(0, "Equipment not found!","Connection", 0)
+
 #Configure the equipment
 R = MB(0, "Do you want to configure the equipment?", "Configure", 4)
 if R == 6:
@@ -28,7 +31,7 @@ if R == 6:
     time.sleep(5)
     inst.write("TPIL:AVER:COUN 10")
     inst.write(":DISP:TPIL:WIND:TRAC:Y:RLEV -20")
-    print("URRA!")
+    print("CONFIGURED!")
     time.sleep(5)
 else:
     MB(0, "END","Configure", 0)
@@ -39,5 +42,17 @@ R = MB(0, "Do you want to normalize the equipment?", "Normalize", 4)
 if R == 6:
         MB(0, "Connect the type N cable! (OUTPUT to INPUT)","Normalize", 0)
         inst.write("CALC:TPIL:NORM")
+        inst.write(":INIT:CONT ON")
+        MB(0, "PRESS==> LOCAL<FREQ<NORMALIZE<CONTINUE","Normalize", 0)
+        MB(0, "Check the normalize!","Normalize", 0)
         time.sleep(30)
+        print("NORMALIZED!")
 
+#colect measure
+
+R = MB(0, "Do you want to measuring now?", "Measuring", 4)
+
+if R == 6:
+        MB(0, "Connect the equipment under test! (OUTPUT to INPUT)","Measuring", 0)
+        M = inst.write(":CALC:DATA?")
+        print(M)
