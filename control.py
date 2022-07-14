dcf126cc58af5c5fd7eca35fd6bca2945cbc4d21
jrocarlos <75398058+jrocarlos@users.noreply.github.com>
2022-07-14 14:47:36 -0300

    includes close button

--- control.py
+++ control.py
@@ -1,17 +1,18 @@
+from cProfile import label
 import tkinter as tk
 import customtkinter
 
-customtkinter.set_appearance_mode("System")
+customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("blue")
 
 root_tk = customtkinter.CTk()  # create CTk window like the Tk window
-root_tk.geometry("400x240")
+root_tk.geometry("400x400")
 
 def button_function():
-    print("button pressed")
+    root_tk.destroy()
 
 # Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=root_tk, command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+button = customtkinter.CTkButton(text="close", master=root_tk, command=button_function)
+button.place(relx=0.35, rely=0.35, anchor=tk.CENTER)
 
 root_tk.mainloop()
