d4ad3fcaa7a2e82049d956be14d79923873834e1
jrocarlos <75398058+jrocarlos@users.noreply.github.com>
2022-07-19 14:39:02 -0300

    create 2nd button

--- control.py
+++ control.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import customtkinter
-import fileB
+
 
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("blue")
@@ -15,10 +15,10 @@ def button_f2():
     root_tk.call()
 
 # Use CTkButton instead of tkinter Button
-button1 = customtkinter.CTkButton(text="close", master=root_tk, command=button_function)
+button1 = customtkinter.CTkButton(text="CLOSE", master=root_tk, command=button_function)
 button1.place(relx=0.50, rely=0.35, anchor=tk.CENTER)
 
-button2 = customtkinter.CTkButton(text="open", master=root_tk, command=button_f2)
+button2 = customtkinter.CTkButton(text="OPEN", master=root_tk, command=button_f2)
 button2.place(relx=0.50, rely=0.55, anchor=tk.CENTER)
 
 root_tk.mainloop()
